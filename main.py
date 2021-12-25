5b94bbbbd0a40f1aca2407488ffcda30527638f8
AymenLaroussi <larousi.aymen@gmail.com>
2021-12-25 15:09:57 +0100

    Looking for matches

--- main.py
+++ main.py
@@ -11,9 +11,27 @@ kp2, des2 = orb.detectAndCompute(img2, None)
 imgKp1 = cv2.drawKeypoints(img1, kp1, None)
 imgKp2 = cv2.drawKeypoints(img2, kp2, None)
 
+# Finding matches
+bf = cv2.BFMatcher()
+matches = bf.knnMatch(des1, des2, k=2)
+# Detecting matches
+good = []
+for m, n in matches:
+    if m.distance < 0.75 * n.distance:
+        good.append([m])
+
+# Printing similarity value
+print(len(good))
+
+# Genrate new image to show matches
+img3 = cv2.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=2)
+
+# Display genrated image
+cv2.imshow('img3', img3)
+
 # Display landmark images
-cv2.imshow('kp1', imgKp1)
-cv2.imshow('kp2', imgKp2)
+# cv2.imshow('kp1', imgKp1)
+# cv2.imshow('kp2', imgKp2)
 
 # Display images
 # cv2.imshow('img1', img1)
